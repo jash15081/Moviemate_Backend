db7f3731fcc42c5032bcc9abe0a3c4275501677b
chintanlad <chintanylad@gmail.com>
2025-01-26 12:59:11 +0530

    test

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = 8000;
+
+app.get('/', (req, res) => 
+{
+    res.send('Hello..It is working..!!!');
+});
+
+app.listen(PORT, () => 
+{
+    console.log(`Server running on http://localhost:${PORT}`);
+});
